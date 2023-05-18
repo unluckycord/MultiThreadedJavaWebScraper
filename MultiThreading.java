08db8c0c8a7c51118b3b81ff7df4b7b7d9651f71
unluckycord <loganknapp2@gmail.com>
2023-05-18 14:59:03 -0700

    Created Basic multithreading and webscraper

--- MultiThreading.java
+++ MultiThreading.java
@@ -1,3 +1,45 @@
 public class MultiThreading {
-    
+    public static void CreatesThreads(int AmountOfThreads) {
+        // Create an array to hold the threads
+        Thread[] threads = new Thread[AmountOfThreads];
+
+        // Create and start the threads
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new MyRunnable("Thread " + (i + 1)));
+            threads[i].start();
+        }
+
+        // Wait for all the threads to finish
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("All threads have finished execution.");
+    }
+
+    static class MyRunnable implements Runnable {
+        private final String name;
+
+        public MyRunnable(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Thread " + name + " started.");
+
+            try {
+                // Simulate some work
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Thread " + name + " finished.");
+        }
+    }
 }
--- Webscraper.java
+++ Webscraper.java
@@ -1,3 +1,35 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+
 public class Webscraper{
-    
+
+    public static void main(String[] args) {
+        try {
+            // Connect to the website and retrieve its HTML
+            Document document = Jsoup.connect("https://apple.com").get();
+
+            // Extract specific elements from the HTML
+            Elements links = document.select("a[href]");
+            Elements images = document.select("img[src]");
+
+            // Print the extracted links
+            System.out.println("Links:");
+            for (Element link : links) {
+                String url = link.attr("abs:href");
+                System.out.println(url);
+            }
+
+            // Print the extracted images
+            System.out.println("\nImages:");
+            for (Element image : images) {
+                String imageUrl = image.attr("abs:src");
+                System.out.println(imageUrl);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
